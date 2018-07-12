bd5dabf804b9aad6b535450ce716436ea74ce5c2
michelle <pistersmichelle@gmail.com>
2018-07-12 13:30:58 +0200

    some exercises functional programming

---
+++ Javascript Algorithms And Data Structures Certification (300 hours)/Functional Programming/slice2.js
@@ -0,0 +1,9 @@
+function nonMutatingSplice(cities) {
+    // Add your code below this line
+    let newerCities = cities.slice(0,3);
+    return newerCities;
+
+    // Add your code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
